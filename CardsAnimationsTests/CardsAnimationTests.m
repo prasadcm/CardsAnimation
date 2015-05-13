3dc4ab9a144bd56d55a34bbe7da492787b9980e4
Prasad Chekkera Muddappa <Prasad.Muddappa@sage.com>
2015-05-13 10:54:03 +0530

    Added Test Animations
    Added Test Animations

---
+++ CardsAnimationsTests/CardsAnimationTests.m
@@ -0,0 +1,40 @@
+//
+//  CardsAnimationsTests.m
+//  CardsAnimationsTests
+//
+//  Created by Prasad CM on 12/05/15.
+//  Copyright (c) 2015 Sonata Software Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CardsAnimationsTests : XCTestCase
+
+@end
+
+@implementation CardsAnimationsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
